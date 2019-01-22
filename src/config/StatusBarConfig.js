3ca2e56d5cfa9e716a3a00981cd524c7524095be
Sammuel Reis <sammugr@hotmail.com>
2019-01-22 13:10:39 -0200

    release

---
+++ src/config/StatusBarConfig.js
@@ -0,0 +1,4 @@
+import { StatusBar } from 'react-native';
+
+StatusBar.setBackgroundColor('#DA552F');  //android
+StatusBar.setBarStyle('light-content');  //ios
---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import { createStackNavigator, createAppContainer } from "react-navigation";
+
+import Main from "./pages/main";
+import Product from "./pages/product";
+
+const AppNavigator = createStackNavigator(
+	{
+		Main,
+		Product
+	},
+	{
+		defaultNavigationOptions: {
+			headerStyle: {
+				backgroundColor: "#DA552f"
+			},
+			headerTintColor: "#FFF"
+		}
+	}
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default AppContainer;
